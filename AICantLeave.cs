bbf64426100461c4365b11b78885c167c0888640
Sebastian Jankowski <41026189+sjankowskim@users.noreply.github.com>
2022-05-30 21:51:51 -0400

    Add files via upload

---
+++ AICantLeave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using ThunderRoad;
+using UnityEngine;
+
+namespace AICantLeave
+{
+    public class AICantLeave : LevelModule
+    {
+        public override IEnumerator OnLoadCoroutine()
+        {
+            EventManager.OnPlayerSpawned += OnPlayerSpawned;
+            Debug.Log("(AICantLeave) Loaded successfully!");
+            return base.OnLoadCoroutine();
+        }
+
+        private void OnPlayerSpawned()
+        {
+            FleePoint.list.Clear();
+        }
+    }
+}
